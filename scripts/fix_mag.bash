b4e550ea0a98beb59e05d6159c7d296f6193f7b3
Marwan Abbas <marwan@ciic.c.catx-ext-efabless.internal>
2022-12-07 06:59:47 -0800

    fixed absolute paths in mag and maglef

---
+++ scripts/fix_mag.bash
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+mag_path=$1
+mags=$(find $mag_path -maxdepth 1 -type f -name '*.mag' )
+
+for mag in $mags; do
+    filename=$(basename $mag)
+    sed -i -E 's#(use +gf180mcu_\S+_sc_(\S+)__\S+ +\S+ +)\S+#\1$PDK_ROOT/$PDK/libs.ref/gf180mcu_fd_sc\2/mag#' $mag_path/$filename
+done
